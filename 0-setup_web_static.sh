df144d180570110603fc3320b3f8f4c9c9577207
Ojobumiche <bumiche@gmail.com>
2023-07-25 15:10:54 +0000

    nginx configured

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Welcome to LabyKreative DotTech" > /data/web_static/releases/test/index.html
+echo "Welcome Joshtech Software Solutions" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
